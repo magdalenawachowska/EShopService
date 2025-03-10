6fb5058ce5f18cec5a4cbeee4e7e080722d096c8
magdalenawachowska <232483@student.uek.krakow.pl>
2025-03-10 03:13:16 +0100

    Dodaj pliki projektów.

---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace EShopService.Models
+{
+    public class Category : BaseModel
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace EShopService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } = default;
+        public string? Ean { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }                                     //ilosc dostepnych sztuk
+        public string? Sku { get; set; }                                   //Stock Keeping Unit- kod do przypisania do produktu w katalogu
+        public Category? Category { get; set; }
+        public bool Deleted { get; set; } = false;                         //okreslenie aktywnosci produktu
+        public DateTime Created_at { get; set; } = DateTime.UtcNow;        //data utowrzenia rekordu
+        public Guid Created_by { get; set; }
+        public DateTime Updated_at { get; set; } = DateTime.UtcNow;        //data ostatniej aktualizacji rekordu
+        public Guid Updated_by { get; set; }                               //identyfikator edytora
+
+
+
+
+    }
+}
