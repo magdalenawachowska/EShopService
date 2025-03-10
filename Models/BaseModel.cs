19778d9011c8d9fef0cfb85c635952d947b543ca
magdalenawachowska <232483@student.uek.krakow.pl>
2025-03-10 15:55:37 +0100

    Lab 1

--- Models/BaseModel.cs
+++ Models/BaseModel.cs
@@ -1,21 +1,12 @@
 ﻿namespace EShopService.Models
 {
     public class BaseModel
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Ean { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }                                     //ilosc dostepnych sztuk
-        public string? Sku { get; set; }                                   //Stock Keeping Unit- kod do przypisania do produktu w katalogu
-        public Category? Category { get; set; }                            //kategoria produktu
+    {                     
         public bool Deleted { get; set; } = false;                         //okreslenie aktywnosci produktu
         public DateTime Created_at { get; set; } = DateTime.UtcNow;        //data utowrzenia rekordu
         public Guid Created_by { get; set; }                               //identyfikator autora
         public DateTime Updated_at { get; set; } = DateTime.UtcNow;        //data ostatniej aktualizacji rekordu
         public Guid Updated_by { get; set; }                               //identyfikator edytora
-
-
 
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,23 +2,15 @@
 
 namespace EShopService.Models
 {
-    public class Product
+    public class Product : BaseModel 
     {
         public int Id { get; set; }
         public string? Name { get; set; } = default;
-        public string? Ean { get; set; }
+        public string? Ean { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public int Stock { get; set; }                                     //ilosc dostepnych sztuk
-        public string? Sku { get; set; }                                   //Stock Keeping Unit- kod do przypisania do produktu w katalogu
+        public int Stock { get; set; } = 0;                                    //ilosc dostepnych sztuk
+        public string? Sku { get; set; } = string.Empty;                      //Stock Keeping Unit- kod do przypisania do produktu w katalogu
         public Category? Category { get; set; }
-        public bool Deleted { get; set; } = false;                         //okreslenie aktywnosci produktu
-        public DateTime Created_at { get; set; } = DateTime.UtcNow;        //data utowrzenia rekordu
-        public Guid Created_by { get; set; }
-        public DateTime Updated_at { get; set; } = DateTime.UtcNow;        //data ostatniej aktualizacji rekordu
-        public Guid Updated_by { get; set; }                               //identyfikator edytora
-
-
-
 
     }
 }
