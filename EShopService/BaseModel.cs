c05d3f4a4f14c93a1957e8dec918282faf12b00f
magdalenawachowska <232483@student.uek.krakow.pl>
2025-03-24 03:19:17 +0100

    Lab 1 & Lab 2 - update

---
+++ EShopService/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EShopService.Models
+{
+    public class BaseModel
+    {                     
+        public bool Deleted { get; set; } = false;                         //okreslenie aktywnosci produktu
+        public DateTime Created_at { get; set; } = DateTime.UtcNow;        //data utowrzenia rekordu
+        public Guid Created_by { get; set; }                               //identyfikator autora
+        public DateTime Updated_at { get; set; } = DateTime.UtcNow;        //data ostatniej aktualizacji rekordu
+        public Guid Updated_by { get; set; }                               //identyfikator edytora
+
+    }
+}
---
+++ EShopService/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace EShopService.Models
+{
+    public class Product : BaseModel 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Ean { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; } = 0;                                    //ilosc dostepnych sztuk
+        public string Sku { get; set; } = string.Empty;                      //Stock Keeping Unit- kod do przypisania do produktu w katalogu
+        public Category Category { get; set; } = default!;
+
+    }
+}
