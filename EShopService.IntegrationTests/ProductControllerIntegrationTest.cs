a0badeaafc6e09312eeebb61506039c8c3ae9c24
magdalenawachowska <232483@student.uek.krakow.pl>
2025-05-12 16:31:48 +0200

    Lab 5.4 ProductController integration test

---
+++ EShopService.IntegrationTests/ProductControllerIntegrationTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using EShop.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Json;
+
+
+
+namespace EShopService.IntegrationTests
+{
+    public class ProductControllerIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+
+        private readonly HttpClient _client;
+        private WebApplicationFactory<Program> _factory;
+
+        public ProductControllerIntegrationTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        // pobranie dotychczasowej konfiguracji bazy danych
+                        var dbContextOptions = services
+                            .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                        //// usunięcie dotychczasowej konfiguracji bazy danych
+                        services.Remove(dbContextOptions);
+
+                        // Stworzenie nowej bazy danych
+                        services
+                         //   .AddDbContext<DataContext>(options => options.UseInMemoryDatabase("MyDBForTest"));              //co z tym inmemory?
+                         .AddDbContext<DataContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ProductsDbTest;Trusted_Connection=True;"));
+
+                    });
+                });
+
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsAllProducts()
+        {
+            //Arrange
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+                dbContext.Products.RemoveRange(dbContext.Products);
+
+                //Stworzenie obiektu
+                dbContext.Products.AddRange(
+
+                    new Product { Name = "Product1" },
+                    new Product { Name = "Product2" });
+
+                //zapisanie obiektu
+                await dbContext.SaveChangesAsync();
+            }
+
+            //Act
+            var response = await _client.GetAsync("/api/product");
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var products = await response.Content.ReadFromJsonAsync<List<Product>>();
+            Assert.Equal(2, products?.Count);
+        }
+
+
+    }
+}
--- EShopService/Program.cs
+++ EShopService/Program.cs
@@ -6,47 +6,59 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using EShop.Domain.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddScoped<ICreditCardService, CreditCardService>();           
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer());       //dodanie kontekstu bazy danych 
-builder.Services.AddInfranstructure(builder.Configuration);
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-
-builder.Services.AddScoped<IProductService, ProductService>();
-
-builder.Services.AddScoped<IEShopSeeder, EShopSeeder>();
-
-builder.Services.AddMemoryCache();
-
-
-var app = builder.Build();
-
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<IEShopSeeder>();
-await seeder.Seed();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace EShopService
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddScoped<ICreditCardService, CreditCardService>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer());       //dodanie kontekstu bazy danych 
+
+            builder.Services.AddInfranstructure(builder.Configuration);
+
+
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            builder.Services.AddScoped<IProductService, ProductService>();
+
+            builder.Services.AddScoped<IEShopSeeder, EShopSeeder>();
+
+            builder.Services.AddMemoryCache();
+
+
+            var app = builder.Build();
+
+            var scope = app.Services.CreateScope();
+            var seeder = scope.ServiceProvider.GetRequiredService<IEShopSeeder>();
+            await seeder.Seed();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+
+            app.Run();
+        }
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-
-app.Run();
 
